30492c2a8947203886ed21b41cdf442958fefbb3
owen <coxowenw@gmail.com>
2023-05-31 17:11:32 -0600

    initial commit

---
+++ .latex/initialize.sh
@@ -0,0 +1,6 @@
+ln -s $HOME/college/.latex/preamble.tex ./preamble.tex
+ln -s $HOME/college/.latex/master.tex ./master.tex
+
+touch ./body.tex
+
+mkdir figures 
---
+++ .latex/transfer.sh
@@ -0,0 +1,7 @@
+$HOME/college/.latex/initialize.sh
+
+kitty nvim notes.tex & nvim body.tex
+
+mkdir notes-backup
+mv notes.* notes-backup
+
